d5e929c72cbaa7812771095af8f0c94be333806f
정현수 <jung660317@naver.com>
2022-02-19 10:55:38 +0900

    chore: 폴더 구조 간단히 정리

---
+++ src/components/Time/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as Styled from './styled';
+
+function Time() {
+  return <Styled.Container />;
+}
+
+export default Time;
---
+++ src/components/Weather/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as Styled from './styled';
+
+function Weather() {
+  return <Styled.Container />;
+}
+
+export default Weather;

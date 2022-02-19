957e761a804590912f706dfbf439ae802ca97982
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-02-19 23:44:12 +0900

    feat : get season and auto set season type

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import styled from "styled-components"
 function App() {
   return (
     <Container>
-      <Season type="winter">
-        <Button>test</Button>
+      <Season>
+        <button>test</button>
       </Season>
     </Container>
   )
@@ -16,10 +16,4 @@ export default App
 
 // 사용자 중심에서 생각하기
 
-const Container = styled.div`
-  width: 100px;
-`
-
-const Button = styled.button`
-  width: 300px;
-`
+const Container = styled.nav``

18579b1c4efde98c015f9368b0362f0a94b9eca7
정현수 <jung660317@naver.com>
2022-02-19 17:22:32 +0900

    feat: 개발 파일 생성

--- dist/Season/index.d.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-interface SeasonProps {
-    animation: string;
-}
-declare const Season: React.FC<SeasonProps>;
-export default Season;
--- dist/Weather/index.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react" />
-declare function Weather(): JSX.Element;
-export default Weather;

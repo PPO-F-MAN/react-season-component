b60f901a1cc1fb8e6bd961cda19b401698c11239
useonglee <useong0830@naver.com>
2022-02-19 21:13:57 +0900

    feat: Season type에 따른 배경 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from "react";
-import { Season, Time, Weather } from "./lib";
+import { Season } from "./lib";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div>
-      <Season animation="animation" />
-      <Time />
-      <Weather />
-    </div>
+    <Container>
+      <Season type="winter">
+        <Button>test</Button>
+      </Season>
+    </Container>
   );
 }
 
 export default App;
+
+// 사용자 중심에서 생각하기
+
+const Container = styled.div`
+  width: 100px;
+`;
+
+const Button = styled.button`
+  width: 300px;
+`;
--- src/lib/types/common.ts
+++ src/lib/types/common.ts
@@ -25,20 +25,7 @@ type TypeChangeTerm = number;
 
 type AnimationRoundTime = number;
 
-type ImagePosition =
-  | "left-top"
-  | "left-center"
-  | "left-bottom"
-  | "right-top"
-  | "right-center"
-  | "right-bottom"
-  | "center-top"
-  | "center-center"
-  | "center-bottom";
+type ImagePosition = "left-top" | "left-center" | "left-bottom" | "right-top" | "right-center" | "right-bottom" | "center-top" | "center-center" | "center-bottom";
 
 // 감싸준 컴포넌트 기준 50%만 이동하도록 해주세요
-type Animation =
-  | "left-to-right"
-  | "right-to-left"
-  | "top-to-bottom"
-  | "bottom-to-top";
+type Animation = "left-to-right" | "right-to-left" | "top-to-bottom" | "bottom-to-top";

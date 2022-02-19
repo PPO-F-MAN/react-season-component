3a33ff83d4f88b8b209d9261aebd069a442507a9
deli-ght <minjju0660@gmail.com>
2022-02-20 02:55:40 +0900

    feat : 애니메이션 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import styled from "styled-components";
-import { Time } from "./lib";
+import React from "react"
+import styled from "styled-components"
+import { Time } from "./lib"
 
 const Container = styled.div`
   display: flex;
@@ -8,34 +8,34 @@ const Container = styled.div`
   align-items: center;
   width: 100vw;
   height: 100vh;
-`;
+`
 
 const Card = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 600px;
-  height: 400px;
-  font-size: 150px;
-`;
+  width: 500px;
+  height: 500px;
+  font-size: 10px;
+`
 
 function App() {
   return (
     <Container>
       <Card>
         <Time
-          type="day"
-          typeChangeTerm={5000}
+          type="auto"
+          typeChangeTerm={100}
           animationRoundTime={6}
-          animation="right-to-left"
+          animation="top-to-bottom"
           imagePosition="right-top"
           background={true}
         >
-          <Card>넹넹넹</Card>
+          <Card>하하하</Card>
         </Time>
       </Card>
     </Container>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/lib/Time/styled.tsx
+++ src/lib/Time/styled.tsx
@@ -1,16 +1,16 @@
-import styled, { css, keyframes } from "styled-components";
+import styled, { css, keyframes } from "styled-components"
 
 interface ImgPositionProps {
-  x: string;
-  y: string;
-  animation: string;
-  duration: number;
-  iteration: number;
+  x: string
+  y: string
+  animation: string
+  duration: number
+  iteration: number
 }
 
 interface ContainerProps {
-  background: boolean;
-  backgroundGradient: string;
+  background: boolean
+  backgroundGradient: string
 }
 
 const leftToRight = keyframes`
@@ -23,7 +23,7 @@ const leftToRight = keyframes`
   100% {
     transform: translateX(0);
   }
-`;
+`
 
 const rightToLeft = keyframes`
   0% {
@@ -35,9 +35,24 @@ const rightToLeft = keyframes`
   100% {
     transform: translateX(0);
   }
-`;
+`
 
 const topToBottom = keyframes`
+  0% {
+    transform: translateY(0);
+    box-shadow: 0 0 100px 100px white;
+  }
+  50% {
+    transform: translateY(-50%);
+    box-shadow: 0 0 100px 50px Rgba(255,244,25,0.3);
+  }
+  100% {
+    transform: translateY(0);
+    box-shadow: 0 0 100px 100px white;
+  }
+`
+
+const bottomToTop = keyframes`
   0% {
     transform: translateY(0);
   }
@@ -47,30 +62,41 @@ const topToBottom = keyframes`
   100% {
     transform: translateY(0);
   }
-`;
+`
 
-const bottomToTop = keyframes`
-  0% {
-    transform: translateY(0);
-  }
-  50% {
-    transform: translateY(-50%);
-  }
-  100% {
-    transform: translateY(0);
-  }
-`;
+const round = keyframes`
+0% {
+  opacity: 0;
+  transform: translate(0,0);
+}
+25% {
+  opacity: 1;
+  transform: translate(100px, -100px);
+}
+50%{
+  opacity: 1;
+  transform: translate(250px ,-250px);
+}
+75%{
+  opacity: 1;
+  transform: translate(400px, -100px);
+}
+100% {
+  opacity: 0;
+  transform: translate(500px, 0);
+}
+`
 
 function getKeyframes(param: string) {
   switch (param) {
     case "left-to-right":
-      return leftToRight;
+      return leftToRight
     case "right-to-left":
-      return rightToLeft;
+      return rightToLeft
     case "top-to-bottom":
-      return topToBottom;
+      return topToBottom
     case "bottom-to-top":
-      return bottomToTop;
+      return bottomToTop
   }
 }
 
@@ -82,11 +108,12 @@ const imgPositionStyle = ({
   iteration,
 }: ImgPositionProps) => css`
   position: absolute;
-  top: ${y === "top" ? "-10%" : y === "bottom" ? "100%" : "50%"};
-  left: ${x === "left" ? "-10%" : x === "right" ? "100%" : "50%"};
-  animation: ${getKeyframes(animation)} ${duration}s ease ${iteration};
+  ${x};
+  ${y};
+  animation: ${getKeyframes(animation)} ${duration}s infinite linear
+    ${iteration}s;
   width: 50%;
-`;
+`
 
 export const Container = styled.div<ContainerProps>`
   background: ${({ background, backgroundGradient }) =>
@@ -97,13 +124,19 @@ export const Container = styled.div<ContainerProps>`
   width: 100%;
   height: 100%;
   z-index: 10;
-`;
+`
 
-export const Image = styled.img`
+export const Image = styled.div`
   ${imgPositionStyle};
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  background: white;
+  border-radius: 50%;
+  box-shadow: 0 0 100px 50px white;
   z-index: -100;
-`;
+`
 
 export const Children = styled.div`
   z-index: 100;
-`;
+`

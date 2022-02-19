0a370149efc4467c0c4452d566c84ac2eb0a221c
정현수 <jung660317@naver.com>
2022-02-19 10:34:21 +0900

    chore: ESLint, Prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'prettier/prettier': 'error',
+  },
+};

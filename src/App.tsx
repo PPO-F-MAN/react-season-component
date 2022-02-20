43ce963fb82dba1091232b4a9bd3d7c417deda1e
정현수 <jung660317@naver.com>
2022-02-20 13:26:22 +0900

    refactor: 사용하지 않는 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
 import React from "react"
-import styled from "styled-components"
-import { Time } from "./lib"
 
 function App() {
-  return (
-    <Container>
-      <Time
-        type="day"
-        animationRoundTime={10}
-        imagePosition="left-bottom"
-        animation="left-to-right"
-        background={true}
-      />
-    </Container>
-  )
+  return <></>
 }
 
 export default App
-
-// 사용자 중심에서 생각하기
-
-const Container = styled.div`
-  margin: 100px;
-  width: 400px;
-  height: 400px;
-`

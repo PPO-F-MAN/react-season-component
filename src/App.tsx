984cb949a7082a7738ed987a1aff1c617d4c1962
정현수 <jung660317@naver.com>
2022-02-20 13:26:22 +0900

    refactor: 사용하지 않는 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
 import React from "react";
-import { Season } from "./lib";
-import styled from "styled-components";
 
 function App() {
-  return (
-    <Container>
-      <Season type="summer">test</Season>
-    </Container>
-  );
+  return <></>;
 }
 
 export default App;
-
-// 사용자 중심에서 생각하기
-
-const Container = styled.nav``;

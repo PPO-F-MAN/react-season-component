8dddfdacc7dcfe932a958ae07405e62953c525f1
useonglee <useong0830@naver.com>
2022-02-20 17:12:32 +0900

    feat: Season 컴포넌트 return 값 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import React from "react";
-<<<<<<< HEAD
 import { Season } from "./lib";
 import styled from "styled-components";
-import { Weather } from "./lib";
-import Sunny from "./lib/Weather/Sunny";
-import Snowy from "./lib/Weather/Snowy";
-import Rainy from "./lib/Weather/Rainy";
-import Cloudy from "./lib/Weather/Cloudy";
 
 function App() {
   return (
     <Container>
-      <Season type="spring">test</Season>
-      {/* <Sunny />
-      <Snowy />
-      {/* <Rainy /> */}
+      <Season type="spring">
+        <div>봄</div>
+      </Season>
+      <Season type="summer">
+        <div>여름</div>
+      </Season>
+      <Season type="autumn">
+        <div>가을</div>
+      </Season>
+      <Season type="winter">
+        <div>겨울</div>
+      </Season>
     </Container>
   );
 }
@@ -24,8 +26,6 @@ export default App;
 // 사용자 중심에서 생각하기
 
 const Container = styled.div`
-top: 0;
-left: 0;
-width: 100px;
-height: 100px;
+  width: 100%;
+  height: 200px;
 `;

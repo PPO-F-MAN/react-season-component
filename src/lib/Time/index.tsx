0a7287c9659a8dfce415c9ed5c1459856769de52
정현수 <jung660317@naver.com>
2022-02-20 12:44:20 +0900

    bug: Time 컴포넌트 에러 수정

--- src/lib/Time/index.tsx
+++ src/lib/Time/index.tsx
@@ -1,69 +1,69 @@
-import React, { useEffect, useState } from "react"
-import * as Styled from "./styled"
-import { Time as TimeProps } from "../types/common"
+import React, { useEffect, useState } from "react";
+import * as Styled from "./styled";
+import { Time as TimeProps } from "../types/common";
 
-const MORNING_GRADIENT = "linear-gradient(90deg, #FFCB87 0%, #FF805D 100%)"
-const DAY_GRADIENT = "linear-gradient(270deg, #63B2F4 0%, #BDD4ED 100%)"
-const EVENING_GRADIENT = "linear-gradient(90deg, #042D60 0%, #456BD7 101.04%)"
-const NIGHT_GRADIENT = "linear-gradient(270deg, #787676 0%, #000000 100%)"
+const MORNING_GRADIENT = "linear-gradient(90deg, #FFCB87 0%, #FF805D 100%)";
+const DAY_GRADIENT = "linear-gradient(270deg, #63B2F4 0%, #BDD4ED 100%)";
+const EVENING_GRADIENT = "linear-gradient(90deg, #042D60 0%, #456BD7 101.04%)";
+const NIGHT_GRADIENT = "linear-gradient(270deg, #787676 0%, #000000 100%)";
 
-const MORNING_IMAGE = "/images/morning_icon.png"
-const DAY_IMAGE = "/images/day_icon.png"
-const EVENING_NIGHT_IMAGE = "/images/night_icon.png"
+const MORNING_IMAGE = "/images/morning_icon.png";
+const DAY_IMAGE = "/images/day_icon.png";
+const EVENING_NIGHT_IMAGE = "/images/night_icon.png";
 
 const Time: React.FC<TimeProps> = ({
   children,
   type = "auto",
   animationRoundTime = 10000,
   typeChangeTerm = 10000,
-  imagePosition,
-  animation,
+  imagePosition = "left-top",
+  animation = "left-to-right",
   background,
 }) => {
-  const [currentType, setCurrentType] = useState<string>(type)
-  const [currentImage, setCurrentImage] = useState<string>(MORNING_IMAGE)
+  const [currentType, setCurrentType] = useState<string>(type);
+  const [currentImage, setCurrentImage] = useState<string>(MORNING_IMAGE);
   const [currentBackground, setCurrentBackground] =
-    useState<string>(MORNING_GRADIENT)
+    useState<string>(MORNING_GRADIENT);
 
-  const [x, y] = imagePosition.split("-")
-  const date = new Date().getHours()
+  const [x, y] = imagePosition.split("-");
+
   useEffect(() => {
-    console.log(date)
+    const date = new Date().getHours();
     if (type === "auto") {
       if (date <= 6) {
-        setCurrentType("night")
+        setCurrentType("night");
       } else if (date <= 12) {
-        setCurrentType("morning")
+        setCurrentType("morning");
       } else if (date <= 18) {
-        setCurrentType("day")
+        setCurrentType("day");
       } else if (date < 20) {
-        setCurrentType("evening")
+        setCurrentType("evening");
       } else {
-        setCurrentType("night")
+        setCurrentType("night");
       }
     }
-  }, [type])
+  }, [type]);
 
   useEffect(() => {
     switch (currentType) {
       case "morning":
-        setCurrentImage(MORNING_IMAGE)
-        setCurrentBackground(MORNING_GRADIENT)
-        break
+        setCurrentImage(MORNING_IMAGE);
+        setCurrentBackground(MORNING_GRADIENT);
+        break;
       case "day":
-        setCurrentImage(DAY_IMAGE)
-        setCurrentBackground(DAY_GRADIENT)
-        break
+        setCurrentImage(DAY_IMAGE);
+        setCurrentBackground(DAY_GRADIENT);
+        break;
       case "evening":
-        setCurrentImage(EVENING_NIGHT_IMAGE)
-        setCurrentBackground(EVENING_GRADIENT)
-        break
+        setCurrentImage(EVENING_NIGHT_IMAGE);
+        setCurrentBackground(EVENING_GRADIENT);
+        break;
       case "night":
-        setCurrentImage(EVENING_NIGHT_IMAGE)
-        setCurrentBackground(NIGHT_GRADIENT)
-        break
+        setCurrentImage(EVENING_NIGHT_IMAGE);
+        setCurrentBackground(NIGHT_GRADIENT);
+        break;
     }
-  }, [currentType])
+  }, [currentType]);
 
   return (
     <Styled.Container
@@ -71,6 +71,7 @@ const Time: React.FC<TimeProps> = ({
       background={background}
     >
       <Styled.Image
+        src={currentImage}
         x={getX(x)}
         y={getY(y)}
         animation={animation}
@@ -79,31 +80,31 @@ const Time: React.FC<TimeProps> = ({
       />
       <Styled.Children>{children}</Styled.Children>
     </Styled.Container>
-  )
-}
+  );
+};
 
-export default Time
+export default Time;
 
 function getX(x: string) {
   switch (x) {
     case "left":
-      return "left : 0;"
+      return "left : 0;";
     case "right":
-      return "right : 0;"
+      return "right : 0;";
     case "center":
-      return "left : 50%; transform : translateX(-50%);"
+      return "left : 50%; transform : translateX(-50%);";
   }
-  return ""
+  return "";
 }
 
 function getY(y: string) {
   switch (y) {
     case "top":
-      return "top : 0;"
+      return "top : 0;";
     case "bottom":
-      return "bottom : 0;"
+      return "bottom : 0;";
     case "center":
-      return "top : 50%; transform : translateY(-50%);"
+      return "top : 50%; transform : translateY(-50%);";
   }
-  return ""
+  return "";
 }

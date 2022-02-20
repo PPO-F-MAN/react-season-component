aad0ff31f79e63eebb16268e716757f05664bd53
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-02-20 13:46:49 +0900

    feat: season 타입별 컴포넌트 분기

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Season } from "./lib";
 import styled from "styled-components";
+import { Weather } from "./lib";
+import Sunny from "./lib/Weather/Sunny";
+import Snowy from "./lib/Weather/Snowy";
+import Rainy from "./lib/Weather/Rainy";
+import Cloudy from "./lib/Weather/Cloudy";
 
 function App() {
   return (
     <Container>
-      <Season type="summer">test</Season>
+      <Season type="spring">test</Season>
+      {/* <Sunny />
+      <Snowy />
+      {/* <Rainy /> */}
     </Container>
   );
 }
@@ -14,4 +22,9 @@ export default App;
 
 // 사용자 중심에서 생각하기
 
-const Container = styled.nav``;
+const Container = styled.div`
+top: 0;
+left: 0;
+width: 100px;
+height: 100px;
+`;
---
+++ src/lib/Season/Summer/styled.ts
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+import typeHandler from "../utils/typeHander";
+
+export const Container = styled.div<{ type: string }>`
+  & .ocean {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+  }
+
+  & .wave {
+    background: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/85486/wave.svg) repeat-x;
+    position: absolute;
+    bottom: 0;
+    width: 3000%;
+    height: 100%;
+    animation: wave 7s cubic-bezier(0.36, 0.45, 0.63, 0.53) infinite;
+    transform: translateY(60%);
+  }
+
+  & .wave:nth-of-type(2) {
+    position: absolute;
+    bottom: -70%;
+    animation: wave 5s cubic-bezier(0.36, 0.45, 0.63, 0.53) -0.125s infinite, swell 7s ease -1.25s infinite;
+    opacity: 1;
+  }
+
+  @keyframes wave {
+    0% {
+      margin-left: 0;
+    }
+    100% {
+      margin-left: -1600px;
+    }
+  }
+
+  @keyframes swell {
+    0%,
+    100% {
+      transform: translate3d(0, -25px, 0);
+    }
+    50% {
+      transform: translate3d(0, 5px, 0);
+    }
+  }
+`;

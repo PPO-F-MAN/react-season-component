19c6f4af3536ef4bcebf592d7874b34000963aef
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-02-20 13:46:49 +0900

    feat: season 타입별 컴포넌트 분기

---
+++ src/lib/Season/Summer/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import * as Styled from "./styled";
+import { Season as SeasonProps } from "../../types/common";
+
+const Summer: React.FC<SeasonProps> = ({ children, type = "auto", typeChangeTerm = 10000, imagePosition, animation }) => {
+  return (
+    <Styled.Container type={type}>
+      <div className="ocean">
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div>{children}</div>
+      </div>
+    </Styled.Container>
+  );
+};
+
+export default Summer;
